1298c3c0fecd097a299b1dc55d4c9f4961a23fb2
Margarita Belyavtseva <rbeliavceva@gmail.com>
2020-04-24 11:40:27 +0300

    Добавляет стили для главного экрана

--- build/js/script.js
+++ build/js/script.js
@@ -1,26 +1,13 @@
 'use strict';
-// var pageHeader = document.querySelector('.page-header');
-// var headerToggle = document.querySelector('.page-header__toggle');
-
-// pageHeader.classList.remove('page-header--nojs');
-
-// headerToggle.addEventListener('click', function () {
-//   if (pageHeader.classList.contains('page-header--closed')) {
-//     pageHeader.classList.remove('page-header--closed');
-//     pageHeader.classList.add('page-header--opened');
-//   } else {
-//     pageHeader.classList.add('page-header--closed');
-//     pageHeader.classList.remove('page-header--opened');
-//   }
-// });
 
 var acc = document.querySelectorAll('.button-show');
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener('click', function () {
-    this.classList.toggle('button-show--active');
-    var panel = this.nextElementSibling;
+  acc[i].addEventListener('click', function (evt) {
+    var button = evt.target;
+    button.classList.toggle('button-show--active');
+    var panel = button.nextElementSibling;
     if (panel.style.display === 'block') {
       panel.style.display = 'none';
     } else {
