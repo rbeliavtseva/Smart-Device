2348973e164f35a69153a2472054c9aa745a1ea3
Margarita Belyavtseva <rbeliavceva@gmail.com>
2020-04-27 14:19:45 +0300

    Реализует открытие/закрытие попапа, доступ с клавиатуры

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,7 @@
 'use strict';
-
+/*
+Реализует аккордеон
+*/
 var acc = document.querySelectorAll('.button-show');
 var i;
 
@@ -15,3 +17,43 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+/*
+Реализует открытие/закрытие попапа
+*/
+var ESC_KEYCODE = 27;
+var headerBtn = document.querySelector('.header__button');
+var closeBtn = document.querySelector('.popup__button-close');
+var popup = document.querySelector('.popup');
+var overlay = document.querySelector('.overlay');
+var textInputs = document.querySelectorAll('.input-field--popup');
+
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var focusMethod = function getFocus() {
+  textInputs[0].focus();
+};
+
+var openPopup = function () {
+  popup.classList.remove('popup--closed');
+  popup.classList.add('popup--open');
+  overlay.style.display = 'block';
+  focusMethod();
+  document.addEventListener('keydown', onPopupEscPress);
+  overlay.addEventListener('click', closePopup);
+};
+
+headerBtn.addEventListener('click', openPopup);
+
+var closePopup = function () {
+  popup.classList.remove('popup--open');
+  popup.classList.add('popup--closed');
+  overlay.style.display = 'none';
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+closeBtn.addEventListener('click', closePopup);
