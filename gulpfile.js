09e17132675f3d6b04e0f8d12e930309ed2351ba
Margarita Belyavtseva <rbeliavceva@gmail.com>
2020-04-29 18:41:40 +0300

    Добавляет минификацию js

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ var svgstore = require("gulp-svgstore")
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
+var minify = require('gulp-minify');
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -56,7 +57,7 @@ gulp.task("images", function() {
       imagemin.svgo()
     ]))
 
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
 
 });
 
@@ -85,7 +86,7 @@ gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source/js/**",
+    // "source/js/**",
     "source//*.ico"
     ], {
       base: "source"
@@ -97,5 +98,11 @@ gulp.task("clean", function () {
   return del("build");
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task('compress', function () {
+  return gulp.src(['source/js/*.js', 'source/js/*.mjs'])
+    .pipe(minify())
+    .pipe(gulp.dest('build/js'))
+});
+
+gulp.task("build", gulp.series("clean", "copy", "css", "compress", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
